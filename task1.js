c074d60117b2a90c631501061a7b356ddae205ad
DavidVardanyan2 <105792904+DavidVardanyan2@users.noreply.github.com>
2022-05-19 17:43:45 +0400

    test3

--- task1.js
+++ task1.js
@@ -109,6 +109,27 @@ for (let i = 2 ; i<=num ; i++) {
 
 alert(sum)
 
+//TASK 7
+
+let chosenNumber = +prompt("Enter a number");
+let n1 = 0; 
+let n2 = 1; 
+let nextNumber;
+
+console.log(n1); 
+console.log(n2); 
+
+nextNumber = n1 + n2;
+
+while (nextNumber <= chosenNumber) {
+
+    console.log(nextNumber);
+
+    nextNumber = n1 + n2;
+    n1 = n2;
+    n2 = nextNumber;
+}
+
 
 
 
